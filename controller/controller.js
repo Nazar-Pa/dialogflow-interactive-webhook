1331ceb849e698267e21f00db82b14e25005421d
Nazar-Pa <nezer.pashayev@gmail.com>
2020-07-25 03:38:10 +0200

    make it better

--- controller/controller.js
+++ controller/controller.js
@@ -31,22 +31,22 @@ const WelcomeIntent = (agent) => {
     //const params = { "template": "checkbox", "items": [{ "uiText": "item 1<br> this item is best", "id": "1" }, { "uiText": "item 2<br> this item is OK", "id": "2" }, { "uiText": "item 3", "id": "3" }], "buttonItems": [{ "uiText": "Yes", "actionText": "process selected", "isPositive": true }, { "uiText": "No", "actionText": "cancel", "isPositive": false }], "align": "h", "size": "l", "eventToCall": "android_event" };
     //const params = { "template": "card", "cardItems": { "imgUrl": "https://picsum.photos/seed/picsum/900/500", "title": "<b>Image Title</b><i>(optional)</i>", "description": "<i>Image description. (optional)</i>" }, "buttonItems": [{ "uiText": "Yes", "actionText": "process selected", "isPositive": true }, { "uiText": "No", "actionText": "cancel", "isPositive": false }], "align": "h", "size": "l", "eventToCall": "android_event" };
     //const params = { "template": "hyperlink", "linkItems": [{ "uiText": "Next Activity", "linkType": "internal", "navigateAndroid": "com.tyagiabhinav.dialogflowchat.NavTestActivity", "navigateIOS": "", "isPositive": true }, { "uiText": "Google", "linkType": "external", "navigateAndroid": "http://www.google.com", "navigateIOS": "http://www.google.com", "isPositive": false }], "align": "v", "size": "l" };
-    //const param_context = { name: "param_context", lifespan: 10, parameters: params };
-    //agent.context.set(param_context);
+    const param_context = { name: "param_context", lifespan: 10, parameters: params };
+    agent.context.set(param_context);
     agent.add('hi am your your personal virtual assistant, here are my capabilities. What is your name?');
 };
 
 const getWeather = (agent) => {
-    const params = { "template": "text" };
+    //const params = { "template": "text" };
     //const param_context = { name: "param_context", lifespan: 10, parameters: params };
     //agent.context.set(param_context);
     agent.add('Tempreature in London is 20 degree');
 };
 
 const activateMotivation = (agent) => {
-    const params = { "template": "text" };
-    const param_context = { name: "param_context", lifespan: 10, parameters: params };
-    agent.context.set(param_context);
+    //const params = { "template": "text" };
+    //const param_context = { name: "param_context", lifespan: 10, parameters: params };
+    //agent.context.set(param_context);
     // randomNum = Math.floor(Math.random()*3);
     for (var a = [0, 1, 2, 3, 4, 6, 7], i = a.length; i--; ) {
         var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
