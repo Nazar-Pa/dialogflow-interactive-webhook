5c5a86ba282b00eb93d8e267d6e43028511eb3de
Nazar-Pa <Np2859290#>
2020-08-11 01:23:00 +0200

    update

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 */
 
 app.post('/webhook', (req, res) => {
-    //const data = req.body;
+    const data = req.body;
+    console.log(data);
     //res.send(data);
     //console.info(`\n\n>>>>>>> S E R V E R   H I T <<<<<<<`);
     handler.WebhookProcessor(req, res);
