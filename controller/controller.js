5f6e556efb4f192a60ddb7cedf46b138cdd28eb7
Nazar-Pa <Np2859290#>
2020-08-11 00:53:43 +0200

    update

--- controller/controller.js
+++ controller/controller.js
@@ -39,7 +39,7 @@ const WelcomeIntent = (agent) => {
 };
 
 const getWeather = (agent) => {
-    const text = request.body.queryResult.queryText;
+    const text = req.body.queryResult.queryText;
     const params = { "template": "text" };
     const param_context = { name: "param_context2", lifespan: 10, parameters: params };
     agent.context.set(param_context);
