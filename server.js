38a25db1c01974524607e0c03b510d48afb50f79
Nazar-Pa <Np2859290#>
2020-08-12 23:32:41 +0200

    update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/webhook', async (req, res) => {
         const params = { "template": "text" };
         const param_context = { name: "param_context4", lifespan: 10, parameters: params };
         agent.context.set(param_context);
-        agent.add(result);
+        agent.add("Sure please fill out");
     };
 
 
