a774cfad8228e9b8eea6f3f5fc6143ad8af22edf
Nazar-Pa <Np2859290#>
2020-10-02 01:02:24 +0200

    commit

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ const androidIntent = (agent) => {
 
     //const params = { "template": "text" };
     const params = { "template": "button", "buttonItems": [{ "uiText": "Food progress", "actionText": "action 1 selected", "isPositive": false }, 
-    { "uiText": "Activity progress", "actionText": "action 2 selected", "isPositive": false }, ], "align": "v", "size": "s", "eventToCall": "android_event" };
+    { "uiText": "Activity progress", "actionText": "action 2 selected", "isPositive": false },], "align": "v", "size": "s", "eventToCall": "android_event" };
     //const params = { "template": "checkbox", "items": [{ "uiText": "item 1<br> this item is best", "id": "1" }, { "uiText": "item 2<br> this item is OK", "id": "2" }, { "uiText": "item 3", "id": "3" }], "buttonItems": [{ "uiText": "Yes", "actionText": "process selected", "isPositive": true }, { "uiText": "No", "actionText": "cancel", "isPositive": false }], "align": "h", "size": "l", "eventToCall": "android_event" };
     //const params = { "template": "card", "cardItems": { "imgUrl": "https://picsum.photos/seed/picsum/900/500", "title": "<b>Image Title</b><i>(optional)</i>", "description": "<i>Image description. (optional)</i>" }, "buttonItems": [{ "uiText": "Yes", "actionText": "process selected", "isPositive": true }, { "uiText": "No", "actionText": "cancel", "isPositive": false }], "align": "h", "size": "l", "eventToCall": "android_event" };
     //const params = { "template": "hyperlink", "linkItems": [{ "uiText": "Next Activity", "linkType": "internal", "navigateAndroid": "com.tyagiabhinav.dialogflowchat.NavTestActivity", "navigateIOS": "", "isPositive": true }, { "uiText": "Google", "linkType": "external", "navigateAndroid": "http://www.google.com", "navigateIOS": "http://www.google.com", "isPositive": false }], "align": "v", "size": "l" };
@@ -141,7 +141,7 @@ const MyFoodProgress = (agent) => {
     agent.add("Your food progress is showing");
 }
 
-const mypActivityrogress = (agent){
+const myActivityrogress = (agent) =>{
     const params = { "template": "text" };
     const param_context = { name: "param_context10", lifespan: 10, parameters: params };
     agent.context.set(param_context);
@@ -167,7 +167,7 @@ const getSteps = (agent) => {
     intentMap.set('ActivateMotivationIntent', activateMotivation);
     intentMap.set('Default Welcome Intent', WelcomeIntent);
     intentMap.set('myFoodProgress', MyFoodProgress);
-    intentMap.set('mypActivityrogress', mypActivityrogress);
+    intentMap.set('mypActivityrogress', myActivityrogress);
     intentMap.set('getSteps', getSteps);
     intentMap.set('AddFoodEntry', addFoodEntry);
     
