0b849bbffe06e908779a440f319639ac92892f83
Nazar-Pa <Np2859290#>
2020-10-06 14:33:34 +0200

    commit

--- server.js
+++ server.js
@@ -44,10 +44,11 @@ app.post('/webhook', async (req, res) => {
 
     const addFoodEntry = async (agent) =>{
         let portion = agent.parameters.portion;
+        let dateTime = agent.parameters.date-time;
         const params = { "template": "text" };
         const param_context = { name: "param_context9", lifespan: 1, parameters: params };
         agent.context.set(param_context);
-        //agent.add(`The portion is: ${portion}`);
+        agent.add(`The portion is: ${portion} on ${dateTime}`);
     }
 
 
