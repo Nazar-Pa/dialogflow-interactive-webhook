4837cd32c1c63b35b12ccb12fd41f263fe24f1be
Nazar-Pa <Np2859290#>
2020-10-20 17:58:39 +0200

    first commit

--- server.js
+++ server.js
@@ -43,8 +43,6 @@ app.post('/webhook', async (req, res) => {
 
 
     const addFoodEntry = async (agent) =>{
-        // let portion = agent.parameters.portion;
-        // let dateTime = agent.parameters.date-time;
         const params = { "template": "text" };
         const param_context = { name: "param_context9", lifespan: 10, parameters: params };
         agent.context.set(param_context);
@@ -123,14 +121,14 @@ app.post('/webhook', async (req, res) => {
     const param_context = { name: "param_context12", lifespan: 2, parameters: params };
     agent.context.set(param_context);
     agent.add("OKAY");
-    }
+    };
 
 
 const androidIntent = (agent) => {
 
     //const params = { "template": "text" };
-    const params = { "template": "button", "buttonItems": [{ "uiText": "Food progress", "actionText": "action 1 selected", "isPositive": false }, 
-    { "uiText": "Activity progress", "actionText": "action 2 selected", "isPositive": false }], "align": "h", "size": "s", "eventToCall": "android_event" };
+    const params = { "template": "button", "buttonItems": [{ "uiText": "Dietary progress", "actionText": "Dietary progress selected", "isPositive": false }, 
+    { "uiText": "Activity progress", "actionText": "Activity progress selected", "isPositive": false }], "align": "h", "size": "s", "eventToCall": "android_event" };
     //const params = { "template": "checkbox", "items": [{ "uiText": "item 1<br> this item is best", "id": "1" }, { "uiText": "item 2<br> this item is OK", "id": "2" }, { "uiText": "item 3", "id": "3" }], "buttonItems": [{ "uiText": "Yes", "actionText": "process selected", "isPositive": true }, { "uiText": "No", "actionText": "cancel", "isPositive": false }], "align": "h", "size": "l", "eventToCall": "android_event" };
     //const params = { "template": "card", "cardItems": { "imgUrl": "https://picsum.photos/seed/picsum/900/500", "title": "<b>Image Title</b><i>(optional)</i>", "description": "<i>Image description. (optional)</i>" }, "buttonItems": [{ "uiText": "Yes", "actionText": "process selected", "isPositive": true }, { "uiText": "No", "actionText": "cancel", "isPositive": false }], "align": "h", "size": "l", "eventToCall": "android_event" };
     //const params = { "template": "hyperlink", "linkItems": [{ "uiText": "Next Activity", "linkType": "internal", "navigateAndroid": "com.tyagiabhinav.dialogflowchat.NavTestActivity", "navigateIOS": "", "isPositive": true }, { "uiText": "Google", "linkType": "external", "navigateAndroid": "http://www.google.com", "navigateIOS": "http://www.google.com", "isPositive": false }], "align": "v", "size": "l" };
